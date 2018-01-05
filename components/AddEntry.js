9a5a35bf63b90870834fba91a922d738de290fa7
christian <cfs.bsi@outlook.com>
2018-01-05 06:45:14 -0200

    feat: Save data on AsyncStore

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import {Ionicons} from '@expo/vector-icons'
 import TextButton from './TextButton'
+import {removeEntry, submitEntry}from '../utils/api'
 
 function SubmitBtn({onPress}) {
     return (
@@ -72,6 +73,7 @@ export default class AddEntry extends Component {
         //Navegate to home
 
         // save on db
+        submitEntry({key, entry})
 
         //clear local notification
     }
@@ -83,7 +85,7 @@ export default class AddEntry extends Component {
 
         //Route to Home
 
-        //UPdate DB
+        removeEntry(key)
     }
 
 
