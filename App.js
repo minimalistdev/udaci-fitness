c667b8582453e5675058c9de188e623aa31e0a91
christian <cfs.bsi@outlook.com>
2018-01-09 10:53:52 -0200

    Create tabs with TabNavigator

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Ionicons} from '@expo/vector-icons'
+import {View, Platform} from 'react-native';
+import AddEntry from './components/AddEntry'
+import {Ionicons, FontAwesome} from '@expo/vector-icons'
 import History from './components/History'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
+import {TabNavigator} from 'react-navigation'
+import {white, purple}from './utils/colors'
+
+const Tabs = TabNavigator({
+    History: {
+        screen: History,
+        navigationOptions: {
+            tabBarLabel: 'History',
+            tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+        }
+    },
+    AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+            tabBarLabel: 'Add Entry',
+            tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+        }
+    }
+}, {
+    navigationOptions: {
+      header:  null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === 'ios' ? white : purple,
+            shadowColor: 'rgba(0, 0, 0, 0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
 
 export default class App extends React.Component {
 
@@ -14,10 +52,10 @@ export default class App extends React.Component {
     render() {
         return (
             <Provider store={createStore(reducer)}>
-            <View style={{flex: 1}}>
-                <View style={{height: 20}}/>
-                <History />
-            </View>
+                <View style={{flex: 1}}>
+                    <View style={{height: 20}}/>
+                    <Tabs />
+                </View>
             </Provider>
         );
     }
