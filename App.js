93c2af715b24a5baa6b39ee0b9e3bd449e96f8d7
christian <cfs.bsi@outlook.com>
2018-01-08 22:57:21 -0200

    Create History and MetricCard

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import {StyleSheet,
-        View
-} from 'react-native';
+import {View} from 'react-native';
 import {Ionicons} from '@expo/vector-icons'
-import  AddEntry from './components/AddEntry'
+import History from './components/History'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
@@ -17,7 +15,8 @@ export default class App extends React.Component {
         return (
             <Provider store={createStore(reducer)}>
             <View style={{flex: 1}}>
-                <AddEntry />
+                <View style={{height: 20}}/>
+                <History />
             </View>
             </Provider>
         );
