9f866692015f739971f59ee2b9a70e266d7ead65
christian <cfs.bsi@outlook.com>
2018-01-11 22:41:49 -0200

    Create Live screen

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {TabNavigator, StackNavigator} from 'react-navigation'
 import {white, purple}from './utils/colors'
 import {Constants} from 'expo'
 import DetailEntry from './components/DetailEntry'
+import Live from './components/Live'
 
 function UdaciStatusBar({backgroundColor, ...props}) {
     return (
@@ -32,6 +33,13 @@ const Tabs = TabNavigator({
         navigationOptions: {
             tabBarLabel: 'Add Entry',
             tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+        }
+    },
+    Live: {
+        screen: Live,
+        navigationOptions: {
+            tabBarLabel: 'Live',
+            tabBarIcon: ({tintColor}) => <Ionicons name='ios-speedometer' size={30} color={tintColor}/>
         }
     }
 }, {
